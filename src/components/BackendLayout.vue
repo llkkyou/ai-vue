9737e239b32dbd2a1436e2f7ee3a0d12272dc321
lky <2927458420@qq.com>
2026-03-21 21:27:21 +0800

    PageHead组件封装

--- src/components/BackendLayout.vue
+++ src/components/BackendLayout.vue
@@ -6,8 +6,8 @@
         <el-header>
           <Navbar />
         </el-header>
-        <el-main>
-          <router-view />
+        <el-main class="main-content">
+          <router-view class="content-container"></router-view>
         </el-main>
       </el-container>
     </el-container>
@@ -24,8 +24,32 @@ import Navbar from './Navbar.vue'
 .backend-layout {
   height: 100vh;
 
+  .el-header {
+    height: 74px !important;
+  }
+
   .main-container {
     height: 100%;
+  }
+
+  // .navbar {
+  //     height: 100%;
+  //     display: flex;
+  //     align-items: center;
+  //     justify-content: space-between;
+  //     padding: 0 15px;
+  //     background: white;
+  //     box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  //     border-bottom: 1px solid #e5e7eb;
+  // }
+  .main-content {
+    flex: 1;
+
+    .content-container {
+      padding: 20px;
+      background-color: #fff;
+      min-height: calc(100% - 74px);
+    }
   }
 }
 </style>
--- src/views/knowledge.vue
+++ src/views/knowledge.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <h1>知识文章</h1>
+    <PageHead title="知识文章">
+      <template #buttons>
+        <el-button type="primary">新增</el-button>
+        <el-button type="primary">删除</el-button>
+      </template>
+    </PageHead>
   </div>
 </template>
+
+<script setup name="Knowledge">
+import PageHead from '@/components/PageHead.vue'
+</script>
