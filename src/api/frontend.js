0eba4ea7f71a1bd5212f0de56552795c32719617
lky <2927458420@qq.com>
2026-03-26 16:10:54 +0800

    报错

--- src/api/frontend.js
+++ src/api/frontend.js
@@ -3,3 +3,11 @@ import service from '@/utils/request'
 export const register = (data) => {
   return service.post('/user/add', data)
 }
+
+export const startSession = (data) => {
+  return service.post('/psychological-chat/session/start', data)
+}
+
+export const sendSessionList = (params) => {
+  return service.get('/psychological-chat/sessions', { params })
+}
